e6d5c5ea941542ed5ab9b205d03d5b32064981d0
Nichol Blackburn <nicholblackburn@outlook.com>
2018-08-31 20:04:13 -0600

    updated view and controllers

---
+++ db/migrate/20180831195143_create_animals.rb
@@ -0,0 +1,13 @@
+class CreateAnimals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.integer :age
+      t.string :hair_color
+      t.string :gender
+      t.boolean :alive
+
+      t.timestamps
+    end
+  end
+end
